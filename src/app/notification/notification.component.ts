55afe67221fcb278c1f3f881a1a59cbfd072c437
HarmeetSingh Sekhon <harmeetsekhon21@gmail.com>
2023-01-09 14:15:16 -0700

    updated code jan 09

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: `<div>
+    <p>This website uses cookies tp ptovide better user experince.</p>
+  </div>`,
+  styleUrls: ['./notification.component.css'],
+})
+export class NotificationComponent {}
